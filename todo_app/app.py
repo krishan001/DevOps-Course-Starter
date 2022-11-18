db47d7d8d0aab995155f25c8c4bb05aba0f8186b
Patel Krishan (ETAS/ERS-ENG1) <Krishan.Patel@etas.com>
2022-11-18 16:02:25 +0000

    adding new items to list

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from todo_app.data.session_items import get_items, add_item
 from todo_app.flask_config import Config
 
@@ -9,6 +9,12 @@ app.config.from_object(Config())
 @app.route('/')
 def index():
     items = get_items()
-    for item in items:
-        add_item(item)
-    return render_template("index.html")
+    item_list = [item.get('title') for item in items]
+    return render_template("index.html", todo_items=item_list)
+
+
+@app.route('/add_new_item', methods=['POST'])
+def add_new_item():
+    item_title = request.form.get('new_item')
+    add_item(item_title)
+    return redirect('/')
