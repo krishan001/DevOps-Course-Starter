a423286b33da3ad1792bcf83d5fa768b0076fd6e
Patel Krishan (ETAS/ERS-ENG1) <Krishan.Patel@etas.com>
2022-11-18 14:55:23 +0000

    adding items to template

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-
+from flask import Flask, render_template
+from todo_app.data.session_items import get_items, add_item
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
@@ -8,4 +8,7 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    items = get_items()
+    for item in items:
+        add_item(item)
+    return render_template("index.html")
