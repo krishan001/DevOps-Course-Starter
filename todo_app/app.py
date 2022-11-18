7f94e9cc4d6f99ae14f84bd5b6bade51604d0b2f
Patel Krishan (ETAS/ERS-ENG1) <Krishan.Patel@etas.com>
2022-11-18 16:25:53 +0000

    sorting item list

--- todo_app/app.py
+++ todo_app/app.py
@@ -9,7 +9,11 @@ app.config.from_object(Config())
 @app.route('/')
 def index():
     items = get_items()
-    item_list = [item.get('title') for item in items]
+    # Sorting items by their status
+    # could just sort in reverse alphabetical order but this allows future additions
+    order = ["Not Started", "Completed"]
+    sorted_items = sorted(items, key=lambda d: order.index(d.get("status")))
+    item_list = [item.get('title') for item in sorted_items]
     return render_template("index.html", todo_items=item_list)
 
 
